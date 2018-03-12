dd498a376a898e2e70f0f37639f500e357988cbf
Julien Sarazin <julien.sarazin@me.com>
2018-03-12 13:51:10 +0100

    reindent

--- models/User.js
+++ models/User.js
@@ -3,25 +3,25 @@ const filePath = './models/Users.json';
 let Users = [];
 
 fs.readFile(filePath, (err, data) => {
-  Users = JSON.parse(data);
+    Users = JSON.parse(data);
 });
 
 function User(data) {
-  this.firstname = data.firstname;
-  this.lastname = data.lastname;
-  this.age = data.age;
+    this.firstname = data.firstname;
+    this.lastname = data.lastname;
+    this.age = data.age;
 }
 
-User.prototype.save = function(cb) {
-  if (!this.firstname || ! this.lastname || !this.age)
-    return cb('missing required properties');
+User.prototype.save = function (cb) {
+    if (!this.firstname || !this.lastname || !this.age)
+        return cb('missing required properties');
 
-  Users.push(this);
-  fs.writeFile(filePath, JSON.stringify(Users), cb);
-}
+    Users.push(this);
+    fs.writeFile(filePath, JSON.stringify(Users), cb);
+};
 
-User.list = function() {
-  return JSON.stringify(Users);
-}
+User.list = function () {
+    return JSON.stringify(Users);
+};
 
 module.exports = User;
