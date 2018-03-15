05194bae19995daa983f88094a7130e132c2214a
Quentin Brunet <hello@quentinbrunet.com>
2018-03-15 12:59:39 +0100

    Clean code + Try to strengthen it :muscle:

--- test/fieldsValidator.js
+++ test/fieldsValidator.js
@@ -2,22 +2,17 @@ function hasProperties(data, properties) {
     if (!data) 
         return properties;
 
-    const splittedProperties = [];
+    const splittedProperties = properties.map(property => property.split('.'));
     const missingProperties = [];
-
-    properties.forEach(property => {
-        splittedProperties.push(property.split('.'));
-    });
 
     splittedProperties.forEach((splittedProperty, i) => {
         let object = data;
-        for (let j = 0; j < splittedProperty.length; ++j) {
+        for (let j = 0, length = splittedProperty.length; j < length; ++j) {
             let property = splittedProperty[j];
             if (property in object) {
                 object = object[property];
-            } else {
-                if (!missingProperties.includes(properties[i]))
-                    missingProperties.push(properties[i]);
+            } else if (!missingProperties.includes(properties[i])) {
+                missingProperties.push(properties[i]);
             }
         }
     });
@@ -26,23 +21,19 @@ function hasProperties(data, properties) {
 }
 
 function whitelist(data, properties) {
-    const splittedProperties = [];
+    const splittedProperties = properties.map(property => property.split('.'));
     
-    properties.forEach(property => {
-        splittedProperties.push(property.split('.'));
-    });
-
     iterate(data, '');
 
     function iterate(obj, trace) {
-        for (prop in obj) {            
-            if (typeof obj[prop] === 'object') {
-                iterate(obj[prop], (trace === '' ? trace + prop : trace + '.' + prop));
-                continue;
-            }
-    
+        for (prop in obj) {
             const tempTrace = (trace === '' ? trace + prop : trace + '.' + prop);
-            
+
+            if (typeof obj[prop] === 'object' && !properties.includes(tempTrace)) {
+                    iterate(obj[prop], (trace === '' ? trace + prop : trace + '.' + prop));
+                    continue;
+            }
+
             if (!properties.includes(tempTrace)) {
                 let object = data;
                 const arrayTrace = tempTrace.split('.');
@@ -62,16 +53,12 @@ function whitelist(data, properties) {
 }
 
 function blacklist(data, properties) {
-    const splittedProperties = [];
-    
-    properties.forEach(property => {
-        splittedProperties.push(property.split('.'));
-    });
+    const splittedProperties = properties.map(property => property.split('.'));
 
     iterate(data, '');
 
     function iterate(obj, trace) {
-        for (prop in obj) {            
+        for (prop in obj) {
             if (typeof obj[prop] === 'object') {
                 iterate(obj[prop], (trace === '' ? trace + prop : trace + '.' + prop));
                 continue;
