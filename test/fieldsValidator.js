28eca4fe6d24fa51d0817236e48b8376df6cf14e
Julien Sarazin <julien.sarazin@me.com>
2018-03-15 10:05:00 +0100

    added tested exercises

---
+++ test/fieldsValidator.js
@@ -0,0 +1,23 @@
+function hasProperty(data, property) {
+
+}
+
+function hasProperties(data, properties) {
+
+}
+
+function whitelist(data, properties) {
+
+}
+
+function blacklist(data, properties) {
+
+}
+
+
+module.exports = {
+    hasProperty,
+    hasProperties,
+    whitelist,
+    blacklist
+};
