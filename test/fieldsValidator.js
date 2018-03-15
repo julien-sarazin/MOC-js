27c0052a19138148bc8db5a3b56e245d28de30a1
unknown <https://github.com/julien-sarazin/MOC-js.git>
2018-03-15 14:25:06 +0100

    changesTest

--- test/fieldsValidator.js
+++ test/fieldsValidator.js
@@ -1,14 +1,83 @@
 function hasProperties(data, properties) {
-    return {}
+    var arr = new Array();
+
+    for(let i=0; i<properties.length; i++){
+        if(data.hasOwnProperty(properties[1]))
+            arr.push(properties[i]);
+    }
+
+    return arr;
 
 }
 
 function whitelist(data, properties) {
+    let nbr = properties.length + 1;
+    for(let i=0; i<nbr; i++) {
+        for (keys in data) {
+            // The key is key
+            // The value is data[key]
+            if (keys === properties[i])
+                delete data[keys];
 
+        }
+    }
 }
 
 function blacklist(data, properties) {
+    function listArr(obj, proper){
+        for (let key in obj) {
+            if(properties.includes(key))
+                delete obj[key];
+            //console.log(key, obj[key]);
+            //console.log('type', typeof(obj[key]));
+            if(typeof(obj[key]) === 'object'){
+                listArr(obj[key]);
+            }
+        }
+    }
+    listArr(data, properties);
 
+    for(let i =0; i<properties.length; i++){
+        if(properties[i].match(/./g))
+            console.log(properties[i]);
+    }
+
+    function getLastProper(obj){
+        for (let key in obj) {
+            console.log(obj);
+            //if (typeof(obj[key]) === 'object') {
+            if(properties[i].match('/./')) {
+                //listArr(obj[key]);
+                console.log('dsdfsdf');
+            }
+        }
+    }
+
+    /*
+    function recursivelyIterateProperties(data, property) {
+
+        if (data instanceof Array) {
+            for (let i = 0; i < data.length; ++i) {
+                recursivelyIterateProperties(data[i])
+            }
+        }
+        else if (typeof(data) === 'object') {
+            for (let proper in data) {//
+                 console.log(proper);
+                console.log('a enlever ' + property);
+                if(proper = property)
+                    delete data[property];
+                if (!(typeof(data[proper]) === 'string')) {
+                    recursivelyIterateProperties(data[proper]);
+                }
+            }
+        }
+    }
+
+    for(let i=0; i<properties.length; i++) {
+        recursivelyIterateProperties(data, properties[i]);
+    }
+    */
 }
 
 
