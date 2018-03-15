16711a16f3a8d1fd6b7af408261744b886c5e2cc
Playdric <cedric.nanteau@gmail.com>
2018-03-15 12:29:24 +0100

    2 test out of 4

--- test/fieldsValidator.js
+++ test/fieldsValidator.js
@@ -1,16 +1,31 @@
 function hasProperties(data, properties) {
-    return {}
+    let res = [];
+    properties.forEach((element) => {
+        let e = element.split('.');
+        if(!data.hasOwnProperty(e[0])) {
+            res.push(element);
+        }
+    });
+    return res
 
 }
 
 function whitelist(data, properties) {
+    for(var propertyName in data) {
+        if(!properties.includes(propertyName))
+            delete data[propertyName]
+    }
+    return data
 
 }
 
 function blacklist(data, properties) {
-
+    properties.forEach((element) => {
+        if(data.hasOwnProperty(element))
+            delete data[element]
+    });
+    return data
 }
-
 
 module.exports = {
     hasProperties,
