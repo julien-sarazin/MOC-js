c376d62d6ec2dd88b343a729785d314bce6beefd
bmhoma <35196379+bmhoma@users.noreply.github.com>
2018-03-15 14:59:16 +0100

    mmm

--- test/fieldsValidator.js
+++ test/fieldsValidator.js
@@ -1,6 +1,18 @@
 function hasProperties(data, properties) {
+	let tab = [];
+	console.log(".");
+	//console.log(data);
+	for(let prop of properties){
+		console.log(data + '.' + prop + ' = ' + data[prop]);
+		console.log('prop = ' + prop);
+		//console.log(data[prop]);
 
+		//console.log('prop = ' + prop);
 
+		if (data[prop] === undefined)
+			tab.push(prop);
+	}
+	return(tab);
 }
 
 function whitelist(data, properties) {
