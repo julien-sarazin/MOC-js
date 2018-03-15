3b9dfd31fb88d8374a80d40cadfac0a30113edd0
Adama <a.traore.pro@gmail.com>
2018-03-15 14:53:14 +0100

    starting testcases

--- test/fieldsValidator.js
+++ test/fieldsValidator.js
@@ -1,16 +1,34 @@
 function hasProperties(data, properties) {
+    let array = [];
+    for (let property of properties) {
+        loopInProperty(data.field, property);
+    }
 
-
+    return array;
 }
 
 function whitelist(data, properties) {
-
+    for(let property in data){
+        if(!(properties.includes(property)))
+            delete data[property];
+    }
+    return data;
 }
 
 function blacklist(data, properties) {
 
 }
 
+function loopInProperty(object, property){
+    let str = property.split('.');
+    let i;
+    let o; //same as Object in Param
+    for(i = 1; i < str.length; i++){
+        if(str in object)
+            return false;
+    }
+    return true;
+}
 
 module.exports = {
     hasProperties,
