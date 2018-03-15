97c6ae02c1e4b24c1512d4bbb34bea3b222ca14a
Léo <leo.legron@gmail.com>
2018-03-15 15:32:44 +0100

    wip

--- test/fieldsValidator.js
+++ test/fieldsValidator.js
@@ -14,27 +14,40 @@ function hasProperty(data, property) {
 
 function hasProperties(data, properties) {
     let notFound = [];
-    properties.forEach(prop => {
-        if (!hasProperty(data, prop)) notFound.push(prop);
-     });
+    const dataIsAnArray = Array.isArray(data);
+    if (dataIsAnArray) {
+        let i = 0;
+        data.forEach(obj => { fillNotFound(obj, dataIsAnArray, properties, notFound, i); i++; })
+    } else {
+        fillNotFound(data, dataIsAnArray, properties, notFound);
+    }
     return notFound;
 }
 
-function whitelist(data, properties) {
-    for (const i in data) {
-        if (!isInObject(i, properties)) delete data[i];
-    }
+function fillNotFound(data, isAnArray, props, array, i) {
+    props.forEach(prop => { if (!hasProperty(data, prop)) array.push(isAnArray ? '[' + i + '].' + prop : prop); });
 }
 
-function isInObject(i, properties) {
+function whitelist(data, properties) {
     let props = [];
     properties.forEach(prop => {
         let split = prop.split('.');
-        props.push(split);
+        split.forEach(e => props.push(e));
     });
-    return props.find(prop => {
-        prop === i
-    });
+    for (const i in data) { if (!isInObject(i, props)) delete data[i]; }
+}
+
+function isInObject(i, props) {
+    try{
+        props.forEach(prop => {
+            if (prop === i) {
+                throw true;
+            }
+        });
+    } catch (e) {
+        return true;
+    }
+    return false;
 }
 
 function blacklist(data, properties) {
