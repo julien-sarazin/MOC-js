50588c94f41c9570eaecd854e76423e93875bd2b
lukile <lucile.1988.ls@gmail.com>
2018-03-15 14:54:02 +0100

    test

--- test/fieldsValidator.js
+++ test/fieldsValidator.js
@@ -1,13 +1,33 @@
 function hasProperties(data, properties) {
+    //should return an Array when passed object contains given properties
+    let array = new Array();
+    if(data.hasOwnProperty(properties)) {
+        array.push(properties);
+    }
+
+    return array;
+
+    //should return missing fields when passed object does not given properties
 
 
+    //should validate each object in the array
 }
 
 function whitelist(data, properties) {
+    //should remove everything from data that is not in given properties array
 
 }
 
 function blacklist(data, properties) {
+   //should remove everything from data that is in given properties array
+
+    for(keys in data){
+        for(key in keys){
+            console.log("keys : " + keys + "\n");
+            console.log("key : " + key + "\n");
+        }
+
+    }
 
 }
 
@@ -17,3 +37,4 @@ module.exports = {
     whitelist,
     blacklist
 };
+
