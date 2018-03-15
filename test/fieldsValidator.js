7acfb9d45814c277be99c5798148a07fc5863e46
lukile <lucile.1988.ls@gmail.com>
2018-03-15 15:32:56 +0100

    test

--- test/fieldsValidator.js
+++ test/fieldsValidator.js
@@ -12,26 +12,23 @@ function hasProperties(data, properties) {
 
 
         //should return missing fields when passed object does not given properties
-    //Doit retourner les champs manquant quand un objet passé n'est pas une propriété
    /*for(keys in data){
        if(!properties.includes(data[keys])){
            return properties[keys];
        }
    }*/
 
-    /*let missingData;
+   /* let missingData = [];
     let propertiesLength = properties.length+1;
     let i = 0;
 
     for(i; i < propertiesLength; i++){
         if(properties[i] != data){
-            missingData = data;
+            missingData.push(data);
         }
     }
     return missingData;*/
 
-
-    return array;
 
     //should return missing fields when passed object does not given properties
 
@@ -42,29 +39,35 @@ function hasProperties(data, properties) {
 function whitelist(data, properties) {
     //should remove everything from data that is not in given properties array'
 
+   /* for(keys in data) {
+        console.log(keys);
+    }
+
     for(keys in data){
         if(!properties.includes(keys)){
             delete data[keys];
         }
     }
     return data;
-
+*/
 
 }
 
+
+
 function blacklist(data, properties) {
-
-
    //should remove everything from data that is in given properties array
 
+
+    /*let length = properties.length + 1;
+
     for(keys in data){
-      
+        for(let i = 0; i < length; i++){
+            delete data[properties[i]];
+        }
+    }*/
 
-    }
 
-    for(let i = 0; i < length; i++){
-        delete data[properties[i]];
-    }
 }
 
 
