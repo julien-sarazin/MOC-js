aaa21a03e968241c4c15cc0ae1802eebcb87633c
Léo <leo.legron@gmail.com>
2018-03-15 14:26:59 +0100

    wip

--- test/fieldsValidator.js
+++ test/fieldsValidator.js
@@ -1,6 +1,23 @@
-function hasProperties(data, properties) {
-    return {}
+function hasProperty(data, property) {
+    let propArray = property.split('.');
+    let newData = data;
+    try{
+        propArray.forEach(prop => {
+            newData = newData[prop];
+            if (newData === undefined) throw 0;
+        });
+    } catch(e){
+        return false;
+    }
+    return true;
+}
 
+function hasProperties(data, properties) {
+    let notFound = [];
+    properties.forEach(prop => {
+        if (!hasProperty(data, prop)) notFound.push(prop);
+     });
+    return notFound;
 }
 
 function whitelist(data, properties) {
@@ -8,11 +25,30 @@ function whitelist(data, properties) {
 }
 
 function blacklist(data, properties) {
+    for (const i in properties) {
+        const globalProp = properties[i];
+        let propArray = globalProp.split('.');
+        let length = propArray.length;
 
+        if (length === 1) delete data[globalProp];
+        else {
+            let newData = data;
+            let count = 1;
+            for (const i in propArray) {
+                const prop = propArray[i];
+                if(length === count){
+                    delete newData[prop]
+                }
+                newData = newData[prop];
+                count++;
+            }
+        }
+    }
 }
 
 
 module.exports = {
+    hasProperty,
     hasProperties,
     whitelist,
     blacklist
