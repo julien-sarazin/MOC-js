2e3918bf79ec2bf10fd09c3856b1d82f7bc76a70
Julien Sarazin <julien.sarazin@me.com>
2018-03-16 10:49:08 +0100

    update config

--- settings/index.js
+++ settings/index.js
@@ -1,3 +1,6 @@
 module.exports = server => {
     server.settings = require('./settings.json');
+
+    server.settings.PORT = process.env.PORT || server.settings.PORT;
+    server.settings.db_uri = process.env.MONGODB_URI || server.settings.db_uri;
 };
