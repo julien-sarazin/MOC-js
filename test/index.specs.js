18c7b3e5e8d83fb0bc65c81a465a4705878138d0
Julien Sarazin <julien.sarazin@me.com>
2018-03-16 10:25:14 +0100

    wip

--- test/index.specs.js
+++ test/index.specs.js
@@ -5,7 +5,7 @@ const should = chai.should();
     describe('Some test', () => {
         it('should fail', () => {
             const a = 'a';
-            const b = 'b';
+            const b = 'a';
 
             return a.should.be.eql(b);
          });
