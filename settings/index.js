1ac736c25ff9cb757bd47cee4d690e86f4d906f7
lukile <lucile.1988.ls@gmail.com>
2018-03-16 10:57:59 +0100

    added conf to travis and deployment

--- settings/index.js
+++ settings/index.js
@@ -1,3 +1,6 @@
 module.exports = server => {
-    server.settings = require('./settings.json');
+    server.settings = require('./settings.json')
+
+    server.settings.port = process.env.PORT || server.settings.PORT
+    server.settings.db_uri = process.env.MONGODB_URI || server.settings.db_uri
 };
